5ee40a5b2f55f47e7f2bec939c241a77bc7bbd13
lixxth <lizbeth78765@gmail.com>
2025-02-13 11:34:43 -0600

    commit inicial

--- Misdatos/src/main/java/com/mycompany/misdatos/Misdatos.java
+++ Misdatos/src/main/java/com/mycompany/misdatos/Misdatos.java
@@ -11,6 +11,7 @@ package com.mycompany.misdatos;
 public class Misdatos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Lizbeth Guadlupe Hernandez Rebolledo");
+          System.out.println("Lizbeth Guadlupe Hernandez Rebolledo");
     }
 }
