2dea4370557fd4ddd145f10339b96d3b985aab35
lixxth <lizbeth78765@gmail.com>
2025-02-13 12:45:14 -0600

    Comentario push

--- Misdatos/src/main/java/com/mycompany/misdatos/Misdatos.java
+++ Misdatos/src/main/java/com/mycompany/misdatos/Misdatos.java
@@ -14,6 +14,6 @@ public class Misdatos {
         System.out.println("Lizbeth Guadlupe Hernandez Rebolledo");
           System.out.println("19012390");
             System.out.println("ingenieria en sistemas computacionales");
-              System.out.println("AAAAAAAAAAAAAAA");
+              System.out.println("Hola vengo con el push"); 
     }
 }
