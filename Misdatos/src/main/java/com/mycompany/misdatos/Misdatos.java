19c4774f06797ddb74ec03ebc43249c010160b52
lixxth <lizbeth78765@gmail.com>
2025-02-13 11:25:08 -0600

    INICIADOR

---
+++ Misdatos/src/main/java/com/mycompany/misdatos/Misdatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.misdatos;
+
+/**
+ *
+ * @author ez pawn
+ */
+public class Misdatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
